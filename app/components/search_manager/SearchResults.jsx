59f1bf3eaa1500b169ecc16bddb0827706459803
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-09-30 16:40:48 -0400

    Basic list of search results for a fixed query

--- app/components/search_manager/SearchResults.jsx
+++ app/components/search_manager/SearchResults.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
 export default class SearchResults extends React.Component {
+    state = {results: []}
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        $.ajax({
+            url: 'search?q=beef',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({results: data.rows});
+            }.bind(this)
+        });
+    }
+
     render() {
         return (
-            <section className="search-results"><div>(TODO)</div></section>
+            <section className="search-results"><div>
+                <ul>{this.state.results.map(row =>
+                    <li><span id="artist">{row.artist}</span>
+                    <span id="title">{row.title}</span></li>)}
+                </ul>
+            </div></section>
         );
     }
 }
