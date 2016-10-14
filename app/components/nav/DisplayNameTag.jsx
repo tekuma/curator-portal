b233ab0df2392680dda102117fe3b591d4b7def5
xstephen95x <stwhite@mit.edu>
2016-10-14 14:26:40 -0400

    skeleton of nav

---
+++ app/components/nav/DisplayNameTag.jsx
@@ -0,0 +1,47 @@
+// Libs
+import React  from 'react';
+
+export default class DisplayNameTag extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        let fontStyle = {
+            fontSize: 16
+        }
+
+        if (this.props.displayName.length > 22) {
+            fontStyle = {
+                fontSize: 9,
+                top: 0
+            }
+        } else if (this.props.displayName.length > 16 ) {
+            fontStyle = {
+                fontSize: 12
+            }
+        }
+
+
+        return (
+            <div
+                className="nav-username"
+                style={{left : this.props.navIsOpen ? 25 : -700}}>
+    			<span
+                    style={fontStyle}
+                    className="nav-username-writing">
+    				{this.props.displayName}
+    			</span>
+    		</div>
+        );
+    }
+}
+
+// ============= PropTypes ==============
+
+DisplayNameTag.propTypes = {
+    displayName: React.PropTypes.string.isRequired,
+    navIsOpen: React.PropTypes.bool.isRequired
+};
