fcde0c69760e7874ede589862f0318289574c56d
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-02 12:25:30 -0400

    Support SQLite as alternative database provider

--- serv/search.js
+++ serv/search.js
@@ -6,34 +6,76 @@
 
 
 var fs = require('fs');
-var mysql = require('mysql');
+var db_provider
 var db;
-exports.connectdb = (dbconf) => {
-    if (dbconf.ssl) {
-        dbconf.ssl.ca = fs.readFileSync(__dirname + '/cert/' + dbconf.ssl.ca);
-        dbconf.ssl.cert = fs.readFileSync(__dirname + '/cert/' + dbconf.ssl.cert);
-        dbconf.ssl.key = fs.readFileSync(__dirname + '/cert/' + dbconf.ssl.key);
+
+exports.connectdb = (dbconf, provider) => {
+    if (typeof provider === 'undefined') {
+        provider = 'mysql';
     }
-    db = mysql.createConnection(dbconf);
-    db.connect();
-    console.log('Connected to MySQL database.');
+
+    if (provider.toLowerCase() === 'sqlite'
+        || provider.toLowerCase() === 'sqlite3') {
+        provider = 'sqlite';  // Normalize provider name
+
+        var sqlite3 = require('sqlite3');
+        db = new sqlite3.Database(':memory:');
+
+    } else if (provider.toLowerCase() === 'mysql') {
+        provider = 'mysql';  // Normalize provider name
+
+        var mysql = require('mysql');
+        if (dbconf.ssl) {
+            dbconf.ssl.ca = fs.readFileSync(__dirname + '/cert/' + dbconf.ssl.ca);
+            dbconf.ssl.cert = fs.readFileSync(__dirname + '/cert/' + dbconf.ssl.cert);
+            dbconf.ssl.key = fs.readFileSync(__dirname + '/cert/' + dbconf.ssl.key);
+        }
+        db = mysql.createConnection(dbconf);
+        db.connect();
+        console.log('Connected to MySQL database.');
+
+    } else {
+        throw new Error('Unrecognized database provider: "'+String(provider)+'"');
+    }
+
+    db_provider = provider;
+    return db;
 }
 
 exports.disconnectdb = () => {
-    db.end();
+    if (db_provider === 'mysql') {
+        db.end();
+    } else {  // === 'sqlite'
+        db.close();
+    }
 }
 
 exports.q = (query) => {
     console.log('Received search query:', query);
     if (query) {
+        if (db_provider === 'mysql') {
+            var query_method = db.query;
+        } else {  // === 'sqlite'
+            var query_method = db.all;
+        }
         return (new Promise(function(resolve, reject) {
             var ex = '%'+query+'%';
-            db.query('SELECT artist, title, description, tags, origin, thumbnail_url FROM `artworks` WHERE LOWER(`artist`) LIKE ? OR LOWER(`title`) LIKE ?',
+
+            if (db_provider === 'mysql') {
+                db.query('SELECT artist, title, description, tags, origin, thumbnail_url FROM `artworks` WHERE LOWER(`artist`) LIKE ? OR LOWER(`title`) LIKE ?',
                      [ex, ex],
                      function (err, rows, fields) {
                          if (err) throw err;
                          resolve(rows);
                      });
+            } else {
+                db.all('SELECT artist, title, description, tags, origin, thumbnail_url FROM `artworks` WHERE LOWER(`artist`) LIKE ? OR LOWER(`title`) LIKE ?',
+                     [ex, ex],
+                     function (err, rows, fields) {
+                         if (err) throw err;
+                         resolve(rows);
+                     });
+            }
         }));
     } else {
         return null;
--- tests/serv/test_search.js
+++ tests/serv/test_search.js
@@ -7,7 +7,14 @@ const dbconf = require('../testdbconf.json');
 describe('search', function() {
 
     beforeEach('Opening database connection', function() {
-        search.connectdb(dbconf);
+        var db = search.connectdb(dbconf, 'sqlite3');
+        db.serialize(function () {
+            db.run('CREATE TABLE artworks (artist TEXT, title TEXT, description TEXT, date_of_addition DATETIME, artist_uid CHAR(255), artwork_uid CHAR(255), date_of_creation DATETIME, tags TEXT, thumbnail_url CHAR(255), origin CHAR(32), reverse_lookup CHAR(255), META TEXT)');
+            var insert_template = db.prepare('INSERT INTO artworks (artist, title, tags) VALUES (?, ?, ?)');
+            insert_template.run(['Scott', 'deadbeef', '32bit,Intel']);
+            insert_template.run(['Scott', 'f00f', 'hex']);
+            insert_template.finalize();
+        });
     });
     afterEach('Closing database connection', function() {
         search.disconnectdb();
