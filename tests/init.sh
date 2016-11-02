96a8c341f047fa4b52e964e2a0490162c035d631
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-02 15:43:38 -0400

    TEST: Improve security by port-forwarding to Docker container only from localhost

--- tests/init.sh
+++ tests/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run -d -p 3306:3306 -e MYSQL_DATABASE=Tekuma_artworkdb -e MYSQL_USER=Tekuma_searcher -e MYSQL_PASSWORD=deadbeef -e MYSQL_RANDOM_ROOT_PASSWORD=yes mysql:latest
+docker run -d -p 127.0.0.1:3306:3306 -e MYSQL_DATABASE=Tekuma_artworkdb -e MYSQL_USER=Tekuma_searcher -e MYSQL_PASSWORD=deadbeef -e MYSQL_RANDOM_ROOT_PASSWORD=yes mysql:latest
 
 while true; do
     sleep 2
