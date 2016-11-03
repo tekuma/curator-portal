cb53069f4d8ba437fb7058b72c5371c3426eb192
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-03 10:03:36 -0400

    TEST: Write SQL keywords in UPPERCASE

--- tests/initdb.sql
+++ tests/initdb.sql
@@ -1,2 +1,2 @@
 use Tekuma_artworkdb;
-create table artworks (artist TEXT, title TEXT, description TEXT, date_of_addition DATETIME, artist_uid CHAR(255), artwork_uid CHAR(255), date_of_creation DATETIME, tags TEXT, thumbnail_url CHAR(255), origin CHAR(32), reverse_lookup CHAR(255), META TEXT);
+CREATE TABLE artworks (artist TEXT, title TEXT, description TEXT, date_of_addition DATETIME, artist_uid CHAR(255), artwork_uid CHAR(255), date_of_creation DATETIME, tags TEXT, thumbnail_url CHAR(255), origin CHAR(32), reverse_lookup CHAR(255), META TEXT);
