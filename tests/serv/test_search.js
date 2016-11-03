46890909818151c129629af0046e03889866f642
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-03 18:18:51 -0400

    TEST: Make test db generation more generic

--- tests/serv/test_search.js
+++ tests/serv/test_search.js
@@ -8,13 +8,17 @@ const dbconf = require('../testdbconf.json');
 
 describe('search', function() {
 
-    before('Opening database connection', function() {
+    before('Opening database connection', function(done) {
         console.log('TEKUMA_TEST_DB: '+process.env.TEKUMA_TEST_DB);
         if (typeof process.env.TEKUMA_TEST_DB !== 'undefined') {
             var db = search.connectdb(dbconf, process.env.TEKUMA_TEST_DB);
         } else {
             var db = search.connectdb(dbconf, 'sqlite');
         }
+
+        const initial_data = [['deadbeef', '4 beef'],
+                              ['f00fba4', 'frozzle']];
+
         if (search.provider() === 'sqlite') {
             db.serialize(function () {
                 const fd = fs.openSync('conf/initdb.sql', 'r');
@@ -26,12 +30,21 @@ describe('search', function() {
 
                     var insert_template = db.prepare('INSERT INTO artworks ' +
                                                      '(uid, title) VALUES (?, ?)');
-                    insert_template.run(['deadbeef', '4 beef']);
-                    insert_template.run(['f00fba4', 'frozzle']);
+                    for (var i = 0; i < initial_data.length; i++) {
+                        insert_template.run(initial_data[i]);
+                    }
                     insert_template.finalize();
 
                 });
             });
+            done();
+        } else {  // === 'mysql'
+            db.query('insert into artworks (uid, title) values (?, ?), (?, ?)',
+                     initial_data[0].concat(initial_data[1]),
+                     function (err, rows, fields) {
+                         if (err) throw done(err);
+                         done();
+                     });
         }
     });
     after('Closing database connection', function() {
