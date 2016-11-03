3fdce6fb333d4aee3874e60bd21f1d37ea65976e
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-03 14:45:28 -0400

    From init script, DROP tables if they already exist

--- conf/initdb.sql
+++ conf/initdb.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS artworks, artists, associations, labels
 CREATE TABLE artworks (uid CHAR(255), title TEXT, description TEXT, date_of_addition DATETIME, date_of_creation DATETIME, thumbnail_url CHAR(255), origin CHAR(32), reverse_lookup CHAR(255), META TEXT)
 CREATE TABLE artists (uid CHAR(255), artist TEXT, human_name TEXT, date_of_addition DATETIME, META TEXT)
 CREATE TABLE associations (label_uid CHAR(255), object_uid CHAR(255), object_table CHAR(255))
