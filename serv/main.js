abef5438cc7310f097e1109f53f0c95fe8a179f1
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-06 16:13:31 -0500

    Allow query directly from command-line

--- serv/main.js
+++ serv/main.js
@@ -4,6 +4,20 @@
 // All rights reserved.
 // created by Scott C. Livingston
 
+var cmd_q_string = null;
+var verbose = false;
+for (let i = 1; i < process.argv.length; i++) {
+    if (process.argv[i] === '-h'
+        || process.argv[i] === '--help') {
+        console.log('Usage: serv/main.js [-h] [-q SEARCH...]');
+        process.exit();
+    } else if (process.argv[i] == '-v') {
+        verbose = true;
+    } else if (process.argv[i] == '-q') {
+        cmd_q_string = process.argv.slice(i+1).join(' ');
+    }
+}
+
 const firebase = require('firebase');
 firebase.initializeApp({
     databaseURL: "https://curator-tekuma.firebaseio.com",
@@ -24,28 +38,39 @@ const search = require('./search.js');
 const dbconf = require(servconf.artworkdb);
 search.connectdb(dbconf);
 
-app.get('/search', function (req, res) {
-    if (!req.query.auth) {
-        res.send('access denied.');
-        return;
-    } else {
+if (cmd_q_string != null) {
 
-        firebase.auth().verifyIdToken(req.query.auth).then(function(decodedToken) {
-            search.q( req.query.q ).then(function(rows) {
-                res.json({rows: rows});
-            });
-        }).catch(function(error) {
-            console.log('firebase auth failed with error: ', error);
+    search.q( cmd_q_string ).then(function(rows) {
+        console.log(rows);
+        process.exit();
+
+    });
+} else {
+
+    app.get('/search', function (req, res) {
+        if (!req.query.auth) {
             res.send('access denied.');
-        });
+            return;
+        } else {
 
-    }
-});
+            firebase.auth().verifyIdToken(req.query.auth).then(function(decodedToken) {
+                search.q( req.query.q ).then(function(rows) {
+                    res.json({rows: rows});
+                });
+            }).catch(function(error) {
+                console.log('firebase auth failed with error: ', error);
+                res.send('access denied.');
+            });
+
+        }
+    });
 
 
-https.createServer({ciphers:'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256',
-                    honorCipherOrder: true,
-                    key: fs.readFileSync(__dirname + '/' + servconf.httpscert.key),
-                    cert: fs.readFileSync(__dirname + '/' + servconf.httpscert.cert),
-                    ca: [fs.readFileSync(__dirname + '/' + servconf.httpscert.ca)]
-                   }, app).listen(servconf.port);
+    https.createServer({ciphers:'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256',
+                        honorCipherOrder: true,
+                        key: fs.readFileSync(__dirname + '/' + servconf.httpscert.key),
+                        cert: fs.readFileSync(__dirname + '/' + servconf.httpscert.cert),
+                        ca: [fs.readFileSync(__dirname + '/' + servconf.httpscert.ca)]
+                       }, app).listen(servconf.port);
+
+}
