096ee4780fec3db7f85b8214c8d97865507f1ba6
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-07 12:39:27 -0500

    Add `info` command to manage.js to get dump of configuration

--- serv/manage.js
+++ serv/manage.js
@@ -25,6 +25,7 @@ assert(cmd_index,
 
 
 if (process.argv[cmd_index] === 'clear') {
+
     const servconf = require('./server-config.json');
     const search = require('./search.js');
     const dbconf = require(servconf.artworkdb);
@@ -32,8 +33,32 @@ if (process.argv[cmd_index] === 'clear') {
     console.log('Resetting database...');
     search.cleardb();
     search.disconnectdb();
+
 } else if (process.argv[cmd_index] === '-h'
            || process.argv[cmd_index] === '--help'
            || process.argv[cmd_index] === 'help') {
-    console.log('Usage: serv/manage.js [-h] COMMAND [OPTIONS...]');
+
+    console.log('Usage: serv/manage.js [-h] COMMAND [OPTIONS...]\n\n'+
+                '    help        Print usage and exit.\n'+
+                '    clear       Reset database; drop tables, create new ones, etc.\n'+
+                '    info        Print summary of backend configuration.\n');
+
+} else if (process.argv[cmd_index] === 'info') {
+    const servconf = require('./server-config.json');
+    const dbconf = require(servconf.artworkdb);
+    const full_config = {
+
+
+    };
+    console.log(JSON.stringify(
+        {
+            servconf: servconf,
+            dbconf: dbconf
+        },
+        null, 4));
+
+} else {
+    console.log( 'Unrecognized command: '+process.argv[cmd_index] );
+    console.log( 'Try `help`.' );
+    process.exitCode = 1;  // Errored usage.
 }
