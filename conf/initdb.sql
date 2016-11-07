bf3171e436f687a96714c7d44dc4f77c143c958d
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-06 21:22:48 -0500

    Make uid columns UNIQUE in CREATE TABLE

--- conf/initdb.sql
+++ conf/initdb.sql
@@ -1,4 +1,4 @@
-CREATE TABLE artworks (uid CHAR(255), title TEXT, description TEXT, date_of_addition DATETIME, date_of_creation DATETIME, thumbnail_url CHAR(255), origin CHAR(32), reverse_lookup CHAR(255), META TEXT)
-CREATE TABLE artists (uid CHAR(255), artist TEXT, human_name TEXT, date_of_addition DATETIME, META TEXT)
+CREATE TABLE artworks (uid CHAR(255) UNIQUE, title TEXT, description TEXT, date_of_addition DATETIME, date_of_creation DATETIME, thumbnail_url CHAR(255), origin CHAR(32), reverse_lookup CHAR(255), META TEXT)
+CREATE TABLE artists (uid CHAR(255) UNIQUE, artist TEXT, human_name TEXT, date_of_addition DATETIME, META TEXT)
 CREATE TABLE associations (label_uid CHAR(255), object_uid CHAR(255), object_table CHAR(255))
-CREATE TABLE labels (uid CHAR(255), term CHAR(255), labeltype CHAR(31))
+CREATE TABLE labels (uid CHAR(255) UNIQUE, term CHAR(255), labeltype CHAR(31))
