7a41025650f398b6f00622c0b29fb63fc3f17550
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-11-07 10:55:38 -0500

    Make `help` a proper command of manage.js

--- serv/manage.js
+++ serv/manage.js
@@ -11,10 +11,28 @@
 // All rights reserved.
 // created by Scott C. Livingston
 
-for (let i = 1; i < process.argv.length; i++) {
-    if (process.argv[i] === '-h'
-        || process.argv[i] === '--help') {
-        console.log('Usage: serv/manage.js [-h] COMMAND [OPTIONS...]');
-        process.exit();
-    }
+const assert = require('assert');
+
+
+var cmd_index = null;
+
+for (let i = 2; i < process.argv.length; i++) {
+    cmd_index = i;
+    break;
+}
+assert(cmd_index,
+       'Some command must be given. Try `help`');
+
+
+if (process.argv[cmd_index] === 'clear') {
+    const servconf = require('./server-config.json');
+    const search = require('./search.js');
+    const dbconf = require(servconf.artworkdb);
+    search.connectdb(dbconf);
+    search.cleardb();
+    search.disconnectdb();
+} else if (process.argv[cmd_index] === '-h'
+           || process.argv[cmd_index] === '--help'
+           || process.argv[cmd_index] === 'help') {
+    console.log('Usage: serv/manage.js [-h] COMMAND [OPTIONS...]');
 }
