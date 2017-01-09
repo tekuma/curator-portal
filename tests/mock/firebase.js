7cd74f11354c24a1cd64683030165df92962c01e
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-01-09 10:20:04 -0500

    Add new (vacuous) capabilities to firebase mock

--- tests/mock/firebase.js
+++ tests/mock/firebase.js
@@ -5,21 +5,83 @@
 // suite outside of this module.
 //
 //
-// Copyright 2016 Tekuma Inc.
+// Copyright 2016, 2017 Tekuma Inc.
 // All rights reserved.
 // created by Scott C. Livingston
 
 const assert = require('assert');
 
 
-exports.initializeApp = () => {}
+exports.initializeApp = (config) => {}
 
 exports.auth = () => {
     return {
-        verifyIdToken: function(authtoken) {
+        verifyIdToken: function (authtoken) {
             return (new Promise(function (resolve, reject) {
                 resolve('deadbeef');
             }));
+        },
+
+        onAuthStateChanged: function (changefcn) {
+            changefcn(true);
+        },
+
+        signInWithEmailAndPassword: function (email, password) {
+            return (new Promise(function (resolve, reject) {
+                resolve('deadbeef');
+            }));
+        },
+
+        signOut: function () {
+            return (new Promise(function (resolve, reject) {
+                resolve();
+            }));
+        },
+
+        currentUser: {
+            uid: 666,
+            getToken: function (x) {
+                return new Promise(function (resolve, reject) {
+                    resolve('deadbeef');
+                });
+            }
+        }
+    };
+}
+
+exports.database = () => {
+    return {
+        ref: function (userpath) {
+            return {
+                once: function (x, oncefcn) {
+                    oncefcn({hasChild: function (uid) { return true; }});
+                },
+
+                set: function (child) { return; },
+
+                push: function () {},
+
+                on: function (x, onfcn) {
+                    onfcn();
+                },
+
+                orderByChild: function (name) {
+                    return {
+                        limitToFirst: function (pageLimit) {
+                            return {
+                                once: function (x) {
+                                    return new Promise(function (resolve, reject) {
+                                        resolve('1337f00f');
+                                    });
+                                }
+                            };
+                        }
+                    };
+                },
+
+                transaction: function(tfcn) {
+                }
+            };
         }
     };
 }
