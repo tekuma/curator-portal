db296f513d71407e8ffde3db60d0b3c9bc05fe02
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-01-11 15:47:10 -0500

    TEST: Test case of no match from get_detail

---
+++ tests/serv/test_search_detail.js
@@ -0,0 +1,50 @@
+// small tests for using `GET /detail` and calling get_detail()
+//
+// Copyright 2017 Tekuma Inc.
+// All rights reserved.
+// created by Scott C. Livingston
+
+
+const assert = require('assert');
+
+const search = require('../../serv/search.js');
+const dbconf = require('../testdbconf.json');
+
+
+describe('getting details about artworks', function() {
+
+    before('Opening database connection', function(done) {
+        console.log('TEKUMA_TEST_DB: '+process.env.TEKUMA_TEST_DB);
+        if (typeof process.env.TEKUMA_TEST_DB !== 'undefined') {
+            var db = search.connectdb(dbconf, process.env.TEKUMA_TEST_DB);
+        } else {
+            var db = search.connectdb(dbconf, 'sqlite');
+        }
+
+        const initial_artworks = [{uid: 'deadbeef', title: '4 beef',
+                                   artist_uid: 'sth14sth'},
+                                  {uid: 'f00fba54', title: 'frozzle',
+                                   artist_uid: 'sth14sth'}];
+
+        const initial_artists = [{uid: 'sth14sth', artist: 'Ricardo', human_name: 'Ricardo'}];
+
+        search.cleardb().then(function () {
+            Promise.all([
+                search.insert_artworks(initial_artworks),
+                search.insert_artists(initial_artists)
+            ]).then( function () { done(); } );
+        });
+    });
+    after('Closing database connection', function() {
+        search.disconnectdb();
+    });
+
+    describe('#get_detail', function() {
+        it('should fail to find artwork with given UID',
+           function() {
+               return search.get_detail('1337f00f').then(function(detail) {
+                   assert( detail.found === false );
+               });
+           });
+    });
+});
