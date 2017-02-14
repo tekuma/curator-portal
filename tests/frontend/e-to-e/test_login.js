755636ccdb841d5c0c1f3c0fd547648b24a289e2
xstephen95x <stwhite@mit.edu>
2017-02-14 18:22:30 -0500

    BUGFIX: testfront webpack url

--- tests/frontend/e-to-e/test_login.js
+++ tests/frontend/e-to-e/test_login.js
@@ -2,32 +2,35 @@
 //
 // Copyright 2017 Tekuma Inc.
 // All rights reserved.
-// created by Scott C. Livingston
+// created by Scott C. Livingston, Afika Nyati, and Stephen White
+
+//NOTE: webpack hosts from port 8000.
+var home = 'https://127.0.0.1:8000';
+// var home = 'https://127.0.0.1';
 
 module.exports = {
-    // modified from pre_auth_home_button.js of the dev-artist-portal repo
-    // by Afika Nyati
-    'Test home button redirects to Tekuma homepage': function (browser) {
-        browser
-            .url('https://127.0.0.1')
-            .waitForElementVisible('body', 2000)
-            .click('a[href="http://tekuma.io"]')
-            .waitForElementVisible('body', 2000)
-            .assert.urlEquals('http://tekuma.io/')
-            .end();
-    },
 
     'Test for text "curator"': function (browser) {
         browser
-            .url('https://127.0.0.1')
+            .url(home)
             .waitForElementVisible('body', 2000)
             .assert.containsText('body', 'Curator')
             .end();
     },
 
+    'Test home button redirects to Tekuma homepage': function (browser) {
+        browser
+        .url(home)
+        .waitForElementVisible('body', 2000)
+        .click('a[href="http://tekuma.io"]')
+        .waitForElementVisible('body', 2000)
+        .assert.urlEquals('http://tekuma.io/')
+        .end();
+    },
+
     'Test for log-in input boxes': function (browser) {
         browser
-            .url('https://127.0.0.1')
+            .url(home)
             .waitForElementVisible('body', 2000)
             .assert.attributeContains('#register-email', 'type', 'email')
             .assert.attributeContains('#register-password', 'type', 'password')
@@ -36,7 +39,7 @@ module.exports = {
 
     'Try to log-in using the TravisCI curator-portal account': function (browser) {
         browser
-            .url('https://127.0.0.1')
+            .url(home)
             .waitForElementVisible('input#register-email', 2000)
             .waitForElementVisible('input#register-password', 1000)
             .setValue('input[type=email]', 'travis-ci@tekuma.io')
